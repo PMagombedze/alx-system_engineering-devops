2509422e3871d3bce32087723b66c8535f63795c
PMagombedze <percymagoras@outlook.com>
2023-11-24 21:53:30 +0200

    reddit api

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Hot Posts
+"""
+
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    resp = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if resp.status_code == 404:
+        return None
+
+    res = resp.json().get("data")
+    after = res.get("after")
+    count += res.get("dist")
+    for c in res.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
